a13f13c684518bd73b005ad5b8adca423112c321
NrmeenAraby <115955249+NrmeenAraby@users.noreply.github.com>
2023-11-30 16:57:01 +0200

    add

--- src/Course.java
+++ src/Course.java
@@ -1,52 +1,84 @@
-/*import java.util.ArrayList;
-//anything
+
+import java.util.ArrayList;
+import java.io.*;
 public class Course {
-    protected  String courseCode;
+    private   String courseCode;
     public  String courseTitle;
     public  int credits;  //>>convert it to string while we deal with files
     public String department;
     public  String description;
+    public Instructor assignedInstructor = new Instructor(); //>>change with real name
+    public ArrayList<StudentGrades> grades=new ArrayList<>();
+    public Assignment assignedAssignment = new Assignment(); //>>change with real name
+    public Quiz assignedQuiz = new Quiz(); //>>change with real name
+    public Midterm assignedMidterm = new Midterm(); //>>change with real name
+    public Final assignedfinal = new Final(); //>>change with real name
+
+    public ArrayList<Student>enrolledStudents=new ArrayList<Student>(); //>>change with real name
+    public String []sessionDates=new String[2];
+
+    Course () {}
+    Course (String courseCode,String courseTitle,int credits,String department,String description,Instructor assignedInstructor
+    Assignment assignedAssignment,Quiz assignedQuiz,Midterm assignedMidterm,Final assignedfinal)
+    {
+        this.courseCode=courseCode;
+        this.courseTitle=courseTitle;
+        this.credits=credits;
+        this.department=department;
+        this.description=description;
+        this.assignedInstructor=assignedInstructor;
+        this.assignedAssignment=assignedAssignment;
+        this.assignedQuiz=assignedQuiz;
+        this.assignedfinal=assignedfinal;
+        this.assignedMidterm=assignedMidterm;
+    }
     public Course(String courseCode,String courseTitle)
     {
         this.courseCode=courseCode;
         this.courseTitle=courseTitle;
     }
-    public Instructor assignedInstructor = new Instructor(); //>>change with real name
-    public Assignment assignedAssignment = new Assignment(); //>>change with real name
-    public Quiz assignedQuiz = new Quiz(); //>>change with real name
-    public Final assignedfinal = new Final(); //>>change with real name
-    public Midterm assignedMidterm = new Midterm(); //>>change with real name
-    public ArrayList<Student>enrolledStudents=new ArrayList<Student>(); //>>change with real name
-    public String []sessionDates=new String[2];
-
-    Course ()
+    public void setCourseCode(String courseCode)
     {
+        this.courseCode=courseCode;
     }
-    public void enrollStudent(Student student,int studentIdx ) //>>student sends me the student and its index
+    public String getCourseCode(String courseCode)
     {
-
+        return  courseCode;
+    }
+    public void enrollStudent(Student student) //>>student sends me the student and its index
+    {
+         enrolledStudents.add(student);
     }
     public  void dropStudent(int studentIdx) //>>student sends me that index
     {
-
+        try {
+            enrolledStudents.remove(studentIdx);
+            }
+        catch (IndexOutOfBoundsException IOBex)
+        {
+            System.out.println(IOBex.getMessage());
+        }
     }
-    public void addAssignedAssignment(Assignment assignment) //>>instructor sends me that assignment
+    public void addAssignedAssignment(Assignment assignedAssignment) //>>instructor sends me that assignment
     {
-
+           this.assignedAssignment=assignedAssignment;
     }
-    public void addAssignedQuiz(Quiz quiz) //>>instructor sends me that quiz
+    public void addAssignedQuiz(Quiz assignedQuiz) //>>instructor sends me that quiz
     {
-
+        this.assignedQuiz=assignedQuiz;
     }
-    public void addAssignedMidterm(Midterm midterm) //>>instructor sends me that Mid
+    public void addAssignedMidterm(Midterm assignedMidterm) //>>instructor sends me that Mid
     {
-
+        this.assignedMidterm=assignedMidterm;
     }
-    public void addAssignedFinal(Final finalExam) //>>instructor sends me that finalExam
+    public void addAssignedFinal(Final assignedfinal) //>>instructor sends me that finalExam
     {
-
+       this.assignedfinal=assignedfinal;
+    }
+    public void setGrades(StudentGrades grade)    //>>Main will send it to me    //array or just a variable?
+    {
+         grades.add(grade);
     }
 
 }
 
-*/
