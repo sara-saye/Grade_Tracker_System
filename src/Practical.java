aa9c01d0a47e948d77a76a63d51ed9e99e4d5ad3
ziad-yasser11 <152395436+ziad-yasser11@users.noreply.github.com>
2023-11-30 23:33:54 +0200

    add test

---
+++ src/Practical.java
@@ -0,0 +1,24 @@
+public class Practical extends Test {
+    private String Practical_Exam_Location;
+    private int Practical_Exam_Time;
+    public Practical(int id, String Title,int max_score ,double mark , String date,String practical_Exam_Location,int practical_Exam_Time){
+        super(id,Title,max_score,mark,date);
+        this.Practical_Exam_Location=practical_Exam_Location;
+        this.Practical_Exam_Time=practical_Exam_Time;
+
+    }
+
+    public void setPractical_Exam_Location(String practical_Exam_Location) {
+        Practical_Exam_Location = practical_Exam_Location;
+    }
+    public void setPractical_Exam_Time(int practical_Exam_Time) {
+        Practical_Exam_Time = practical_Exam_Time;
+    }
+    public int getPractical_Exam_Time() {
+        return Practical_Exam_Time;
+    }
+
+    public String getPractical_Exam_Location() {
+        return Practical_Exam_Location;
+    }
+}
