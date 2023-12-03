5b5939063790822ee66e9bd4f0dcd34f723d2b91
Sara_Darwish <112208033+sara-saye@users.noreply.github.com>
2023-12-03 21:14:23 +0200

    ..

--- src/Student.java
+++ src/Student.java
@@ -5,12 +5,12 @@ public class Student extends Person {
 
     static Scanner input = new Scanner(System.in);
 
-    private final Course[] Student_courses = new Course[48];
-    private final StudentGrades[] grades = new StudentGrades[48];
+    public final Course[] Student_courses = new Course[48];
+    public final StudentGrades[] grades = new StudentGrades[48];
     private int NoOfCourses=0;
     private double expenses = 0;
     private boolean expenses_paid = false;
-    public boolean[] attendance = new boolean[];
+    public boolean[][] attendance = new boolean[10][2];
     public Student (){
         super();
     }
