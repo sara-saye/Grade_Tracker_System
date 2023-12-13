c73cd061537f7b9dcf46251a5726b19e0f533c8a
Marwan_Elyamany <127440058+1MaRo1@users.noreply.github.com>
2023-12-13 16:22:44 +0200

    Changed arr to arrlist and added event and fixed get fn in test class

--- src/Notification.java
+++ src/Notification.java
@@ -3,12 +3,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Notification {
-private ArrayList<Assignment> assignment = new ArrayList<Assignment>();
-private  ArrayList<Quiz> quiz = new ArrayList<Quiz>();
-private boolean New_grade;
-private boolean attendance;
-private double gpa;
-private String Event;
+        private ArrayList<Assignment> assignment = new ArrayList<Assignment>();
+        private  ArrayList<Quiz> quiz = new ArrayList<Quiz>();
+    private boolean New_grade;
+    private boolean attendance;
+    private double gpa;
+    private String Event;
+    public int warningCounter = 0;
+
 
     public void addAssignedAssignment(Assignment assignedAssignment) //>>instructor sends me that assignment
     {
@@ -16,57 +18,104 @@ private String Event;
             assignment.add(assignedAssignment);
         }
     }
+
+//    public void addAssignment(int ID){
+//        for (int studentIndex=0;studentIndex<Main.students.size();studentIndex++){
+//            if (ID==Main.students.get(studentIndex).getID()){
+//                for (int studentCoursesIndex =0;studentCoursesIndex<Main.students.get(studentIndex).Student_courses.size();studentCoursesIndex++){
+//                    for (int courseIndex =0; courseIndex<Main.courses.size();courseIndex++) {
+//                        if (Main.courses.get(courseIndex).getCourseCode().equals(Main.students.get(studentIndex).Student_courses.get(studentCoursesIndex).getCourseCode())) {
+//                            if (!Main.courses.get(courseIndex).assignedAssignment.isEmpty()) {
+//                                for (int assignmentIndex = 0; assignmentIndex < Main.courses.get(courseIndex).assignedAssignment.size(); assignmentIndex++) {
+//                                    this.assignment.add(Main.courses.get(courseIndex).assignedAssignment.get(assignmentIndex));
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+
     public void addAssignedQuiz(Quiz assignedQuiz) //>>instructor sends me that quiz
     {
         for (int i=0;i<= this.quiz.size();i++) {
             quiz.add(assignedQuiz);
         }
     }
+
     public void addStatueOfGrade(boolean new_grade) //>>I get it from Grades class
     {
-        this.New_grade=new_grade;
-    }
-    public void addAttendance(boolean attendance){
-        this.attendance=attendance;
-    }
-    public void addGpa(double gpa){
-        this.gpa=gpa;
-    }
-    public void addEvent(String event){
-        this.Event=event;
+        this.New_grade = new_grade;
     }
 
+    public void addAttendance(boolean attendance) {
+        this.attendance = attendance;
+    }
 
+    public void addGpa(double gpa) {
+        this.gpa = gpa;
+    }
 
-public void Display_Notification(){
-        if(!assignment.isEmpty()) {
-            for (int i = 0; i <= this.assignment.size(); i++) {
-                System.out.println(assignment.get(i).getTitle());
-                System.out.println("Start Date: " + assignment.get(i).getAssignment_startDate());
-                System.out.println("Deadline: " + assignment.get(i).getAssignment_Deadline());
-                Duration diff = Duration.between(assignment.get(i).getAssignment_startDate().atStartOfDay(), assignment.get(i).getAssignment_Deadline().atStartOfDay());
+    public void addEvent(String event) {
+        this.Event = event;
+    }
+
+    public void Display_Notification(ArrayList<Course> course, int studentID) {
+        for (int courseIndex=0;courseIndex <Main.students.get(studentID).Student_courses.size();courseIndex++) {
+            course.add(Main.students.get(studentID).Student_courses.get(courseIndex));
+        }
+        if (!course.isEmpty()) {
+            for (int i = 0; i <= course.size(); i++) {
+                System.out.println(course.get(i).assignedAssignment.get(i).getTitle());
+                System.out.println("Start Date: " + course.get(i).assignedAssignment.get(i).getAssignment_startDate());
+                System.out.println("Deadline: " + course.get(i).assignedAssignment.get(i).getAssignment_Deadline());
+                Duration diff = Duration.between(course.get(i).assignedAssignment.get(i).getAssignment_startDate().atStartOfDay(), course.assignedAssignment.get(i).getAssignment_Deadline().atStartOfDay());
                 long diffDays = diff.toDays();
                 System.out.println("Assignment ends at " + diffDays + " days");
             }
         }
-    if(!quiz.isEmpty()) {
-        for (int i = 0; i <= this.quiz.size(); i++) {
-            System.out.println(quiz.get(i).getTitle() + " " + quiz.get(i).getDate());
+        for (int i = 0; i <= course.size(); i++) {
+            System.out.println(course.get(i).assignedQuiz.get(i).getTitle() + " " + course.get(i).assignedQuiz.get(i).getDate());
         }
+
+        if (this.New_grade) {
+            System.out.println("Your grades have been updated!");
+            this.New_grade = false;
+        }
+
+//        if (attendance) {
+//            System.out.println("Warning content");
+//            warningCounter++;
+//            this.attendance = false;
+//        }
+
+        if (Main.students.get(studentID).getGPA() <= 1.5) {
+            System.out.println("Warning content");
+        }
+
+        if (!Event.equals("NULL")) {
+            System.out.println(Event);
+        }
+
     }
-    if (this.New_grade){
-        System.out.println("Your grades have been updated");
+
+
+    public void dropGPAStudent(int studentID) {
+
+        if (Main.students.get(studentID).getGPA() < 1) {
+
+            Main.students.get(studentID).setGpaDrop(true);
+        }
+
     }
-    if (attendance){
-        System.out.println("Warning content");
+
+    public void dropAttendance(int studentID){
+        if (warningCounter == 3){
+            Main.students.get(studentID).setAttendanceDrop(true);
+        }
+
     }
-    if (gpa <= 1.5&&gpa!=-1){
-        System.out.println("Warning content");
-    }
-    if (Event != null){
-        System.out.println(Event);
-    }
-}
     public String NotificationToString(){
         String a=New_grade+","+attendance+","+gpa+","+Event;
         if(!assignment.isEmpty()) {
